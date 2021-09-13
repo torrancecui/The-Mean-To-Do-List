2177c4fe70bf84bb858e179bbc00e6fb02ca3700
Torrance Cui <cuitorrance@gmail.com>
2021-09-13 15:40:44 -0700

    basic structure

--- the-mean-to-do-list-app/src/App.js
+++ the-mean-to-do-list-app/src/App.js
@@ -1,23 +1,34 @@
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function Todo({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      {todo.text}
+    </div>
+  );
+};
+
+function App() {
+  const [todos,setTodos] = React.useState([
+    { text: "1"},
+    { text: "2"},
+    { text: "3"}
+  ]);
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
