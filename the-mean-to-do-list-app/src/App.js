29ce804689887fcb7c1afd6d4aec5fba86f8ca50
Torrance Cui <cuitorrance@gmail.com>
2021-09-14 17:02:12 -0700

    able to add new to dos

--- the-mean-to-do-list-app/src/App.js
+++ the-mean-to-do-list-app/src/App.js
@@ -1,10 +1,9 @@
 
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-
-function Todo({ todo }) {
+function ToDo({ todo }) {
   return (
     <div className="todo">
       {todo.text}
@@ -12,22 +11,53 @@ function Todo({ todo }) {
   );
 };
 
+function ToDoForm({addToDo}){
+  const [value, setValue] = React.useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addToDo(value);
+    setValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+};
+
 function App() {
-  const [todos,setTodos] = React.useState([
+  //argument in useState is default
+  //todos is the state variable
+  //setTodos we use to set the state variable
+  const [todos, setTodos] = React.useState([
     { text: "1"},
     { text: "2"},
     { text: "3"}
   ]);
+
+  const addToDo = text => {
+    //copy old todos and add new to do, change state of old todos to new todos using setTodos
+    const newToDos = [...todos, { text }];
+    setTodos(newToDos);
+  };
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo
+          <ToDo
             key={index}
             index={index}
             todo={todo}
           />
         ))}
+        <ToDoForm addToDo={addToDo} />
       </div>
     </div>
   );
